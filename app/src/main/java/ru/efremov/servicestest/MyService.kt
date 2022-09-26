44d4d9c6619dd77ed22808653d2f0ecc79681b0f
ae <alexandr.efremoff@gmail.com>
2022-09-26 21:40:48 +0300

    Notification initial.

--- app/src/main/java/ru/efremov/servicestest/MyService.kt
+++ app/src/main/java/ru/efremov/servicestest/MyService.kt
@@ -18,13 +18,14 @@ class MyService: Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         log("onStartCommand")
+        val start = intent?.getIntExtra(EXTRA_START, 0) ?: 0
         coroutineScope.launch {
-            for (i in 0..100) {
+            for (i in start..start + 100) {
                 delay(1000)
                 log("timer: $i")
             }
         }
-        return super.onStartCommand(intent, flags, startId)
+        return START_STICKY
     }
 
     override fun onBind(p0: Intent?): IBinder? {
@@ -43,8 +44,12 @@ class MyService: Service() {
 
     companion object {
 
-        fun newIntent(context: Context): Intent {
-            return Intent(context, MyService::class.java)
+        private const val EXTRA_START = "start"
+
+        fun newIntent(context: Context, start: Int): Intent {
+            return Intent(context, MyService::class.java).apply {
+                putExtra(EXTRA_START, start)
+            }
         }
     }
 }
