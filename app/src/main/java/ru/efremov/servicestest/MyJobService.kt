c94bce97cb19c728ccf320dbe6a430c0ba01f415
ae <alexandr.efremoff@gmail.com>
2022-10-03 16:13:14 +0300

    JobService refactoring.

--- app/src/main/java/ru/efremov/servicestest/MyJobService.kt
+++ app/src/main/java/ru/efremov/servicestest/MyJobService.kt
@@ -5,7 +5,9 @@ import android.app.job.JobParameters
 import android.app.job.JobService
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
+import android.os.PersistableBundle
 import android.util.Log
 import kotlinx.coroutines.*
 
@@ -18,14 +20,23 @@ class MyJobService: JobService() {
         log("onCreate")
     }
 
-    override fun onStartJob(p0: JobParameters?): Boolean {
+    override fun onStartJob(params: JobParameters?): Boolean {
         log("onStartCommand")
-        coroutineScope.launch {
-            for (i in 0..100) {
-                delay(1000)
-                log("timer: $i")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = params?.dequeueWork()
+                while (workItem != null) {
+                    val page = workItem.intent?.getIntExtra(PAGE, 0)
+
+                    for (i in 0..5) {
+                        delay(1000)
+                        log("timer: $i $page")
+                    }
+                    params?.completeWork(workItem)
+                    workItem = params?.dequeueWork()
+                }
+                jobFinished(params, false)
             }
-            jobFinished(p0, true)
         }
         return true
     }
@@ -48,5 +59,18 @@ class MyJobService: JobService() {
     companion object {
 
         const val JOB_ID = 111
+        private const val PAGE = "page"
+
+        fun newBundle(page: Int): PersistableBundle {
+            return PersistableBundle().apply {
+                putInt(PAGE, page)
+            }
+        }
+
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
     }
 }
